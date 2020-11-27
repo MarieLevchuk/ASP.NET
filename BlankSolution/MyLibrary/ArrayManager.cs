3ed26b018b257210a71c507d12be550569c82519
Мария <marie.levchuk@gmail.com>
2020-11-27 18:24:41 +0300

    addad class library and class ArrayManager

---
+++ BlankSolution/MyLibrary/ArrayManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public static class ArrayManager
+    {
+        public static int FindMax(int[] arr)
+        {
+            int max = arr[0];
+            for(int i=0; i< arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+        }
+    }
+}
