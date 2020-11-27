b256fefd064e4437fc01fb555d487d1c0ecefd6e
Мария <marie.levchuk@gmail.com>
2020-11-27 18:43:52 +0300

    added a test to the FindMax method

---
+++ BlankSolution/MyLibraryTest/ArrayManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+using System;
+
+namespace MyLibraryTest
+{
+    [TestClass]
+    public class ArrayManagerTests
+    {
+        [TestMethod]
+        public void FindMax_2345_5Returned()
+        {
+            //arrange
+
+            int[] arr = { 2, 3, 4, 5 };
+            int expected = 5;
+
+            //act
+
+            int actual = ArrayManager.FindMax(arr);
+
+            //assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
