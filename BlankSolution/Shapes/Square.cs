c40c842285fffb3ae3a0e3766c764c7c627bcf98
Мария <marie.levchuk@gmail.com>
2020-12-04 18:15:55 +0300

    added an example of applying the Liskov Substitution Principle

---
+++ BlankSolution/Shapes/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Square: Figure
+    {
+        public double Side { get; set; }
+
+        public Square (string name, double side) : base(name)
+        {
+            Side = side;
+        }
+
+        public override double GetArea()
+        {
+            return Side * Side;
+        }
+    }
+}
