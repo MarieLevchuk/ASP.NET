f9e661f6434457e6c6c1e24d77d2bcb2b10fcb98
Мария <marie.levchuk@gmail.com>
2020-12-04 22:07:01 +0300

    added IoC

---
+++ BlankSolution/WebApplication_IoC/Reader/DesktopReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_IoC.Reader
+{
+    public class DesktopReader: IReader
+    {
+        public void OpenBook()
+        {
+            //some code to open e-book locally
+        }
+    }
+}
