66140ca70aed9cfb45ecdf003a09b3bc1dfdb5c5
Мария <marie.levchuk@gmail.com>
2021-01-20 17:32:40 +0300

    added DAL

---
+++ BlankSolution/LoginPage/Models/UsersDAL.cs
@@ -0,0 +1,23 @@
+﻿using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+using static LinqToDB.Reflection.Methods.LinqToDB;
+
+namespace LoginPage.Models
+{
+    public class UsersDAL
+    {
+        string _connectionString = "Data Source=HOME-1;Initial Catalog=UsersDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<User> GetUsers()
+        {
+            List<User> usersList = new List<User>();
+            DataContext dataContext = new DataContext(_connectionString);
+            Table<User> users = dataContext.GetTable<User>();
+
+            return usersList;
+        }
+    }
+}
