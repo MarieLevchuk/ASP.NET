a04ce3cb9c13f98bfedec748f14ba19a71d32004
Мария <marie.levchuk@gmail.com>
2021-02-15 22:29:41 +0300

    added database class

---
+++ BlankSolution/ajax/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ajax.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Shop(int id)
+        {
+            var allShops = ShopsDB.Shops;
+            if (id != 0 && allShops.Count>0)
+            {
+                return PartialView(allShops);
+            }
+            return HttpNotFound();
+        }
+    }
+}
---
+++ BlankSolution/ajax/Models/Shop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ajax.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WorkTime { get; set; }
+        public string Description { get; set; }
+    }
+}
