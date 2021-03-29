da858a5426df105071a790d4689d8d62a5b4da0c
Мария <marie.levchuk@gmail.com>
2021-03-29 13:03:46 +0300

    added NumberOfTrailingZeros

---
+++ BlankSolution/JosephusPermutation/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JosephusPermutation
+{
+    class Program
+    {
+        //public static List<object> JosephusPermutation(List<object> items, int k)
+        //{
+        //    List<object> newSequence = new List<object>();
+        //    int selected = 0;
+        //    bool stop = false;
+        //    int start = 0;
+
+        //    if (!stop)
+        //    {
+        //        newSequence.Add(items[k]);
+        //    }
+
+        //    if (stop)
+        //        return newSequence;
+        //}
+
+        public static int JosephusPermutation(int n, int k)
+        {
+            int res = 0;
+            for (int i = 1; i <=n; ++i)
+            {
+                res = (res + k) % i;
+            }
+            return res + 1;
+        }
+        public static List<object> JosephusPermutation(List<object> items, int k)
+        {
+            List<object> newSequence = new List<object>();
+            //int  = items.Count;
+            int length = items.Count;
+            int res = 0;
+            while (items.Count != 0)
+            {
+                //res = (res + k) % i;
+                
+                if ((res + k) > items.Count)
+                {
+                    res = items.Count - res + 1;                    
+                }
+                else
+                {
+                    res += k;
+                }                
+
+                newSequence.Add(items[res - 1]);
+                items.RemoveAt(res - 1);
+                res--;
+            }
+            return newSequence;
+        }
+        static void Main(string[] args)
+        {
+            List<object> items = new List<object> { 1, 2, 3, 4, 5, 6, 7 };
+            foreach (var item in items)
+            {
+                Console.Write(item + "  ");
+            }
+            Console.WriteLine("\n");
+
+            int l = 3;
+            List<object> newSequence = JosephusPermutation(items, l);
+
+            foreach (var item in newSequence)
+            {
+                Console.Write(item + "  ");
+            }
+
+            int n = 14;
+            int k = 2;
+            //Console.WriteLine("the chosen place is" + JosephusPermutation(n,k));
+
+        }
+    }
+}
---
+++ BlankSolution/NumberOfTrailingZeros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberOfTrailingZeros
+{
+    class Program
+    {
+        public static int CountZeros(int n)
+        {
+            int res = 0;
+
+            for (int i = n; i > 0; i--)
+            {
+                if (i % 5 == 0)
+                {
+                    int count = i;
+                    while (count % 5 == 0)
+                    {
+                        res++;
+                        count /= 5;
+                    }
+                }
+            }
+
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountZeros(6));
+        }
+    }
+}
